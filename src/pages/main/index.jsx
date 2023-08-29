20f71e1696f5d0881cc7ce683c88cf3213d55d15
R5G1 <vlad.klimachevski@gmail.com>
2023-08-29 16:23:01 +0300

    feat: add filters ShowArray

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -12,38 +12,54 @@ function Main() {
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(1);
 
-  async function fatchPosts() {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
-      params: { _limit: limit, _page: page },
-    });
-    setPost(response.data);
-    const xTotalCount = Number(response.headers['x-total-count']);
-    setTotalCount(getPagesCount(xTotalCount, limit));
-  }
+  // async function fatchPosts() {
+  //   const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+  //     params: { _limit: limit, _page: page },
+  //   });
+  //   setPost(response.data);
+  //   const xTotalCount = Number(response.headers['x-total-count']);
+  //   setTotalCount(getPagesCount(xTotalCount, limit));
+  // }
 
-  function clikPage(params) {
-    setPage(params);
-  }
+  // function clikPage(params) {
+  //   setPage(params);
+  // }
 
-  useEffect(() => {
-    fatchPosts();
-  }, [page]);
+  // useEffect(() => {
+  //   fatchPosts();
+  // }, [page]);
 
   function creatArray() {
-    function randomInteger(min, max) {
+    function randomInteger(min, max, role = 'false') {
       const rand = min + Math.random() * (max + 1 - min);
-      return Math.floor(rand);
+      if (role === 'true') {
+        return Math.floor(rand) < 10 ? '0' + Math.floor(rand) : Math.floor(rand);
+      } else {
+        return Math.floor(rand);
+      }
     }
     const arraayText = ['Курьер', 'Курьер Лавка', 'Сборщик'];
+    const arrayTown = [
+      'Москва',
+      'Санкт-Петербург',
+      'Новосибирск',
+      'Екатеринбург',
+      'Нижний Новгород',
+      'Казань',
+      'Челябинск',
+      'Омск',
+      'Самара',
+      'Ростов-на-Дону',
+    ];
 
     const array = [];
     for (let i = 0; i < 100; i++) {
       array.push({
         userId: i + Math.random(),
-        town: 'text',
+        town: arrayTown[randomInteger(0, 9)],
         leadType: arraayText[randomInteger(0, 2)],
-        registeredFrom: `${randomInteger(1, 31)}.07.2023`,
-        registeredTo: `${randomInteger(1, 30)}.08.2023`,
+        registeredFrom: `${randomInteger(1, 31, 'true')}.07.2023`,
+        registeredTo: `${randomInteger(1, 31, 'true')}.08.2023`,
         priority: randomInteger(1, 10),
         rate: randomInteger(1000, 5000),
       });
@@ -58,7 +74,7 @@ function Main() {
       <div className={style.mainConteiner}>
         <ShowArray post={rateArray} />
       </div>
-      <div className={style.mainConteinerPages}>
+      {/* <div className={style.mainConteinerPages}>
         {getPagesArray(totalCount).map((item) => (
           <button
             onClick={() => clikPage(item)}
@@ -68,7 +84,7 @@ function Main() {
             {item}
           </button>
         ))}
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/pages/main/mainComponents/showArray.jsx
+++ src/pages/main/mainComponents/showArray.jsx
@@ -1,17 +1,98 @@
 import { useState } from 'react';
-import { IArray } from '../../../components/type/type';
-import createContent from './createContent';
 import FilterArray from './filterArray';
 import styles from './style/showArray.module.scss';
 import Modal from '../../../components/modal/modal';
+import { format, parseISO, parse, isAfter, isBefore } from 'date-fns';
 
 function ShowArray({ post }) {
   const [modalActive, setModalActive] = useState(false);
-  const [string, setString] = useState();
-  // console.log(toString(string));
-  function handleClick() {}
+
+  const [townFilter, setTownFilter] = useState('');
+  const [leadTypeFilter, setLeadTypeFilter] = useState('');
+  const [priorityFilter, setPriorityFilter] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+
+  const [filteredPosts, setFilteredPosts] = useState(post); // Ваш массив данных
+
+  // Функция для применения фильтров
+  const applyFilters = () => {
+    let filteredArray = post;
+
+    // Применение фильтра по leadType
+    filteredArray = filteredArray.filter((item) => item.leadType === 'Курьер Лавка');
+
+    // Применение фильтра по priority
+    filteredArray = filteredArray.filter((item) => item.priority === Number(priorityFilter));
+
+    // Другие фильтры можно добавить по аналогии
+
+    setFilteredPosts(filteredArray);
+  };
+
+  const filteredPostsTest = post.filter((item) => {
+    function registeredDate() {
+      if (!startDate && !endDate) {
+        return true;
+      }
+
+      const registeredFromDate = parse(item.registeredFrom, 'dd.MM.yyyy', new Date());
+      const registeredToDate = parse(item.registeredTo, 'dd.MM.yyyy', new Date());
+      const selectedFromDate = parse(startDate, 'yyyy-MM-dd', new Date());
+      const selectedToDate = parse(endDate, 'yyyy-MM-dd', new Date());
+
+      return selectedFromDate <= registeredFromDate && selectedToDate >= registeredToDate;
+    }
+
+    function townMatches() {
+      if (townFilter !== '') {
+        return item.town.toLowerCase().includes(townFilter.toLowerCase());
+      } else {
+        return item.town;
+      }
+    }
+
+    function leadTypeMatches() {
+      if (leadTypeFilter !== '') {
+        return item.leadType.toLowerCase().includes(leadTypeFilter.toLowerCase());
+      } else {
+        return item.leadType;
+      }
+    }
+
+    function priorityMatches() {
+      if (priorityFilter !== '') {
+        return item.priority === Number(priorityFilter);
+      } else {
+        return item.priority;
+      }
+    }
+
+    return registeredDate() && townMatches() && leadTypeMatches() && priorityMatches();
+  });
+
   return (
     <div>
+      <div>
+        <input
+          type="text"
+          placeholder="Фильтр по городу"
+          onChange={(e) => setTownFilter(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Фильтр по типу лида"
+          onChange={(e) => setLeadTypeFilter(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Фильтр по приоритету"
+          onChange={(e) => setPriorityFilter(e.target.value)}
+        />
+        <input type="date" id="dateInputFrom" onChange={(e) => setStartDate(e.target.value)} />
+        <input type="date" id="dateInputTo" onChange={(e) => setEndDate(e.target.value)} />
+      </div>
+      <button onClick={applyFilters}>Применить фильтры</button>
       <table className={styles.ShowArrayContetn}>
         <thead>
           <tr>
@@ -37,33 +118,16 @@ function ShowArray({ post }) {
         </thead>
 
         <tbody>
-          <tr
-            onClick={() => {
-              setModalActive(false);
-            }}
-          >
-            {/* <td className={styles.ShowArrayContetnUserId}>
-              <FilterArray array={createContent(post, 'userId')} useFilter={false} />
-            </td> */}
-            <td>
-              <FilterArray array={createContent(post, 'town')} />
-            </td>
-            <td>
-              <FilterArray array={createContent(post, 'leadType')} />
-            </td>
-            <td>
-              <FilterArray array={createContent(post, 'registeredFrom')} />
-            </td>
-            <td>
-              <FilterArray array={createContent(post, 'registeredTo')} />
-            </td>
-            <td>
-              <FilterArray array={createContent(post, 'priority')} />
-            </td>
-            <td>
-              <FilterArray array={createContent(post, 'rate')} />
-            </td>
-          </tr>
+          {filteredPostsTest.map((item, index) => (
+            <tr key={item.userId.toString()}>
+              <td>{item.town}</td>
+              <td>{item.leadType}</td>
+              <td>{item.registeredFrom}</td>
+              <td>{item.registeredTo}</td>
+              <td>{item.priority}</td>
+              <td>{item.rate}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <Modal active={modalActive} setActive={setModalActive}>
